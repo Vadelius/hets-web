a1a890d2fdc7f50b8e565261ef1361c62cbbf258
Victor Vadelius <victorvadelius@gmail.com>
2020-03-01 18:27:44 +0100

    Revert "Shared Lint & TSConfig."
    This reverts commit cee38380479c3b18b75f4647d9d9ef21d79a94ef.

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,10 +1,8 @@
-import { NestFactory } from "@nestjs/core";
-
-import { AppModule } from "./app.module";
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
-	const app = await NestFactory.create(AppModule);
-	await app.listen(3000);
+  const app = await NestFactory.create(AppModule);
+  await app.listen(3000);
 }
-// eslint-disable-next-line @typescript-eslint/no-floating-promises
 bootstrap();
